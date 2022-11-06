d1722cb3c5f0b24f18178cb0f03ae47b60dbdeae
ChandraKarthik07 <99110756+ChandraKarthik07@users.noreply.github.com>
2022-11-06 15:34:33 +0530

    Update diabetesfinal.py

--- diabetesfinal.py
+++ diabetesfinal.py
@@ -1,10 +1,12 @@
 import pip
-k=[]
-for i in k:
+import pkg_resources
+l=['cloudpickle==2.2.0', 'customtkinter==2.3', 'joblib==1.0.1', 'matplotlib==3.4.3', 'matplotlib-inline==0.1.3', 'numpy==1.21.2', 'packaging==21.0', 'pandas==1.3.3', 'pathlib==1.0.1', 'pickle4==0.0.1', 'pickle5==0.0.11', 'pickles==0.1.1', 'pickleshare==0.7.5', 'Pillow==8.4.0', 'requests==2.28.1', 'scikit-build==0.12.0', 'scikit-learn==1.0', 'scipy==1.7.1', 'seaborn==0.11.2', 'sklearn==0.0', 'urllib2-file==0.2.1', 'urllib3==1.26.12']
+for package in l:
     try:
-        __import__(i)
-    except ImportError:
-        pip.main(['install', i])
+        dist = pkg_resources.get_distribution(package)
+        print("ccg")
+    except pkg_resources.DistributionNotFound:
+        pip.main(['install',package])
 import joblib
 import pandas as pd
 import cloudpickle as cp
